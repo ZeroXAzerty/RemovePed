f23d64dfe033b9739b18898ea11503b263d7e9b3
ZeroXAzerty <87154152+ZeroXAzerty@users.noreply.github.com>
2021-11-06 04:40:37 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,12 +1,3 @@
-function text(content)
-    SetTextFont(1)
-    SetTextProportional(0)
-    SetTextScale(1.2, 1.2)
-    SetTextEntry("STRING")
-    AddTextComponentString(content)
-    DrawText(0.87, 0.8)
-
-end
 
 Citizen.CreateThread(function()
 
@@ -20,16 +11,4 @@ Citizen.CreateThread(function()
 
 end)
 
-Citizen.CreateThread(function()
-    local ped = PlayerPedId()
-    while true do
-        Citizen.Wait(0)
-        local bool, player = GetEntityPlayerIsFreeAimingAt(ped)
-        ped = GetPlayerName(source)
-        print(player)
-        text("Vous visez sur "..ped)
-
-
-    end
-end)
 
